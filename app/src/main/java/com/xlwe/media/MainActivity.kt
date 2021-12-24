ee87ffa7f9c5f34849ed9350983aa0b300086c73
ilya <ilya.komendantov@list.ru>
2021-12-24 18:51:31 +0300

    start

---
+++ app/src/main/java/com/xlwe/media/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.xlwe.media
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.xlwe.media.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
